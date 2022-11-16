8183fc4af183d692b2a76b6f0ef26961fbb2adbc
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-11-16 23:15:32 +0100

    Update LineTool.cs

--- Draw.Base/Shapes/LineTool.cs
+++ Draw.Base/Shapes/LineTool.cs
@@ -11,7 +11,7 @@ public class LineTool : Tool
 
     public override void Pressed(CanvasShape canvasShape, Control control, PointerPressedEventArgs e)
     {
-        if (!Equals(e.Pointer.Captured, control) && _lineShape is null)
+        if (_lineShape is null)
         {
             _lineShape = new LineShape
             {
@@ -22,24 +22,21 @@ public class LineTool : Tool
 
             canvasShape.Children.Add(_lineShape);
 
-            e.Pointer.Capture(control);
-
             control.InvalidateVisual();
         }
     }
 
     public override void Released(CanvasShape canvasShape, Control control, PointerReleasedEventArgs e)
     {
-        if (Equals(e.Pointer.Captured, control) && _lineShape is { })
+        if (_lineShape is { })
         {
             _lineShape = null;
-            e.Pointer.Capture(null);
         }
     }
 
     public override void Moved(CanvasShape canvasShape, Control control, PointerEventArgs e)
     {
-        if (Equals(e.Pointer.Captured, control) && _lineShape is { })
+        if (_lineShape is { })
         {
             _lineShape.End = e.GetPosition(control);
             control.InvalidateVisual();
